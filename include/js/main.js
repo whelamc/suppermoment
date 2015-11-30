616844d83a6782b4b0fa9e289024da9345ad3c73
whelam <whelamc@users.noreply.github.com>
2015-11-30 09:37:15 +0800

    Update main.js

--- include/js/main.js
+++ include/js/main.js
@@ -77,10 +77,10 @@ BRUSHED.slider = function(){
 		thumb_links				:	0,			// Individual thumb links for each slide
 		thumbnail_navigation    :   0,			// Thumbnail navigation
 		slides 					:  	[			// Slideshow Images
-											{image : 'include/img/slider-images/image01.jpg', title : '<div class="slide-content">Supper Moment</div>', thumb : '', url : ''},
-											{image : 'include/img/slider-images/image02.jpg', title : '<div class="slide-content">Supper Moment</div>', thumb : '', url : ''},
-											{image : 'include/img/slider-images/image03.jpg', title : '<div class="slide-content">Supper Moment</div>', thumb : '', url : ''},
-											{image : 'include/img/slider-images/image04.jpg', title : '<div class="slide-content">Supper Moment</div>', thumb : '', url : ''}  
+											{image : 'include/img/slider-images/image01.jpg', title : '<div class="slide-content"><h1>Supper Moment</h1></div>', thumb : '', url : ''},
+											{image : 'include/img/slider-images/image02.jpg', title : '<div class="slide-content"><h1>Supper Moment</h1></div>', thumb : '', url : ''},
+											{image : 'include/img/slider-images/image03.jpg', title : '<div class="slide-content"><h1>Supper Moment</h1></div>', thumb : '', url : ''},
+											{image : 'include/img/slider-images/image04.jpg', title : '<div class="slide-content"><h1>Supper Moment</h1></div>', thumb : '', url : ''}  
 									],
 									
 		// Theme Options			   
